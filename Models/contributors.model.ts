aab34a32df914d354f166970651a34d8de4b3f28
romualdascode <45950302+romualdascode@users.noreply.github.com>
2019-04-26 13:41:31 +0300

    Add files via upload
    contributors

---
+++ Models/contributors.model.ts
@@ -0,0 +1,12 @@
+export interface Contributors {
+	Id: number;
+	DocTopicId: number;
+	DocExampleId: number;
+	UserId: number;
+	DocContributorTypeId: number;
+	CreationDate:  string;
+	DeletionDate: string;
+	DocContributorDeletionReasonId: number;
+};
+
+
