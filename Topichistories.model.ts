211581471cde84e8be32da29660730a2353d78cc
oskarrr991 <oskarrr991@gmail.com>
2019-04-26 13:33:23 +0300

    Addeded Topichistories.model.ts

---
+++ Topichistories.model.ts
@@ -0,0 +1,11 @@
+export interface Topichistories {
+	
+    Id: number;
+    DocTopicHistoryTypeId: number;
+    DocTagId: number;
+    DocTopicId: number;
+    CreationDate: string;
+    RevisionNumber: number;
+    CreationUserId: number;
+    Text: string;
+};
